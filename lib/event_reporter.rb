42c2c0965287635a852cf7486b1472ebb6c66c3f
Persa Zula <persa@persazula.com>
2013-09-17 12:02:27 -0600

    Delete everything cuz Katrina told me so. Cry. Then add new tests.

--- lib/event_reporter.rb
+++ lib/event_reporter.rb
@@ -1,25 +1,10 @@
 require 'csv'
 
 class EventReporter
+  attr_reader :filename
 
   def initialize
-    puts "Event Reporter initialized."
-  end
-
-  def load_csv(filename)
-    if filename == nil
-      default = 'event_attendees.csv'
-      contents = CSV.open default, headers: true, header_converters: :symbol
-      return default
-    else
-      contents = CSV.open filename, headers: true, header_converters: :symbol
-      return filename
-    end
-    # contents.each do |row|
-    #   id = row[0]
-    #   name = row[:first_name]
-    #   phone = clean_phone_number(row[:homephone])
-    #   zipcode = clean_zipcode(row[:zipcode])
+    @filename = "event_attendees.csv"
   end
 
 end   
--- test/happy_path_test.rb
+++ test/happy_path_test.rb
@@ -1,23 +1,14 @@
-require 'minitest'
+gem 'minitest'
 require 'minitest/autorun'
+require 'minitest/pride'
 require './lib/event_reporter'
 
-class HappyPathTest < Minitest::Test
-  attr_accessor :reporter 
 
-  def setup
-    @reporter = EventReporter.new
-  end 
+class EventReporterTest < Minitest::Test 
 
-  def test_will_load_file_if_nil
-    filename = nil
-    assert_equal "event_attendees.csv", reporter.load_csv(filename)
+  def test_default_filename
+    reporter = EventReporter.new
+    assert_equal "event_attendees.csv", reporter.filename
   end
 
-  def test_will_load_file_with_name
-    filename = "event_attendees_copy.csv"
-    assert_equal filename, reporter.load_csv(filename)
-  end
-
-
-end 
+end
