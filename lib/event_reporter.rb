8a411dbbcdfa9bcba28dde3bb65cedab54b13241
Persa Zula <persa@persazula.com>
2013-09-18 13:02:12 -0600

    Adds tests for filename and first_name

--- lib/event_reporter.rb
+++ lib/event_reporter.rb
@@ -1,10 +1,40 @@
 require 'csv'
 
+
+class Attendee
+  attr_reader :first_name, :last_name
+
+  def initialize(data)
+    @first_name = data[:first_name]
+    @last_name = data[:last_name]
+  end
+end
+
 class EventReporter
   attr_reader :filename
+  # attr_accessor :first_name, :last_name, :phone_number
 
-  def initialize
-    @filename = "event_attendees.csv"
+  def initialize(filename=nil)
+    @filename = filename || "event_attendees.csv"
   end
 
-end   
+  def attendees
+    result = []
+    contents.each do |row|
+      result << Attendee.new(row)
+    end
+    result
+  end
+
+  private
+
+  def contents
+    @contents ||= load_data
+  end
+
+  def load_data
+    CSV.open filename, headers: true, header_converters: :symbol
+  end
+
+end
+
--- test/happy_path_test.rb
+++ test/happy_path_test.rb
@@ -3,6 +3,20 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/event_reporter'
 
+class AttendeeTest < Minitest::Test
+
+  def test_attendee_attributes
+    data = {:first_name => "Joe", 
+            :last_name => "Smith", 
+            :phone_number => "3306669955" }
+    attendee = Attendee.new(data)
+    assert_equal "Joe", attendee.first_name
+    assert_equal "Smith", attendee.last_name
+  end
+
+end
+
+
 
 class EventReporterTest < Minitest::Test 
 
@@ -10,5 +24,34 @@ class EventReporterTest < Minitest::Test
     reporter = EventReporter.new
     assert_equal "event_attendees.csv", reporter.filename
   end
+
+  def test_default_filename_when_passing_nil_filename
+    reporter = EventReporter.new(nil)
+    assert_equal "event_attendees.csv", reporter.filename
+  end
+
+  def test_user_provided_filename
+    reporter = EventReporter.new("fake.csv")
+    assert_equal "fake.csv", reporter.filename
+  end
+
+  def test_find_first_name
+    reporter = EventReporter.new("./test/fixtures/fake.csv")
+    attendee = reporter.attendees.first
+    assert_equal "Allison", attendee.first_name
+    assert_equal "Nguyen", attendee.last_name
+  end
+
+
+  ###
+  ###  Once I figure out how to store the data in a hash, 
+  ###  some tests about finding some folks named John 
+  ###  and counting them goes here
+  ###
+
+
+
+
+
 
 end
